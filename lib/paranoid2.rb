af2f35f654736dff06e48acf0f289332495776da
akicho8 <akicho8@gmail.com>
2015-04-06 19:06:59 +0900

    Extract Paranoid2 module scope methods to paranoid2/base.rb

--- lib/paranoid2.rb
+++ lib/paranoid2.rb
@@ -5,50 +5,6 @@ require 'active_record'
 
 require 'paranoid2/persistence'
 require 'paranoid2/scoping'
+require 'paranoid2/base'
 
-module Paranoid2
-  extend ActiveSupport::Concern
-
-  class_methods do
-    def paranoid?
-      false
-    end
-
-    def paranoid
-      include Persistence
-      include Scoping
-    end
-
-    alias acts_as_paranoid paranoid
-
-    def with_paranoid(options = {})
-      forced = options[:force] || paranoid_force
-      previous, self.paranoid_force = paranoid_force, forced
-      return yield
-    ensure
-      self.paranoid_force = previous
-    end
-
-    def paranoid_force=(value)
-      Thread.current['paranoid_force'] = value
-    end
-
-    def paranoid_force
-      Thread.current['paranoid_force']
-    end
-  end
-
-  def paranoid?
-    self.class.paranoid?
-  end
-
-  def paranoid_force
-    self.class.paranoid_force
-  end
-
-  def with_paranoid(value, &block)
-    self.class.with_paranoid value, &block
-  end
-end
-
-ActiveSupport.on_load(:active_record) { include Paranoid2 }
+ActiveSupport.on_load(:active_record) { include Paranoid2::Base }
