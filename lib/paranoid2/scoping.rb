35cbf85e9a8a43f6dc7e88842938c597b44c2203
akicho8 <akicho8@gmail.com>
2015-04-07 19:02:36 +0900

    Rewrite class methods to named scope

--- lib/paranoid2/scoping.rb
+++ lib/paranoid2/scoping.rb
@@ -3,20 +3,10 @@ module Paranoid2
     included do
       # Affect to default value of deleted_at when create.
       default_scope { paranoid_scope }
-    end
 
-    class_methods do
-      def paranoid_scope
-        where(deleted_at: Paranoid2.alive_value)
-      end
-
-      def only_deleted
-        with_deleted.where.not(deleted_at: Paranoid2.alive_value)
-      end
-
-      def with_deleted
-        unscope(where: :deleted_at)
-      end
+      scope :paranoid_scope, -> { where(deleted_at: Paranoid2.alive_value) }
+      scope :only_deleted,   -> { with_deleted.where.not(deleted_at: Paranoid2.alive_value) }
+      scope :with_deleted,   -> { unscope(where: :deleted_at) }
     end
   end
 end
